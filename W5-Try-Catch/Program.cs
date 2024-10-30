21ae325b0741bd8c43bbafe97c9f5e468f6ccea8
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-30 16:42:36 +0300

    Proje dosyası ekle.

---
+++ W5-Try-Catch/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        bool Value = false;
+        while (!Value)
+        {
+            try
+            {
+                Console.WriteLine("Lütfen bir sayı değeri giriniz.");
+                int cevap = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Girilen sayının karesi:" + cevap * cevap);
+                Value = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz formatta bir  giriş yaptınız! Lütfen bir sayı giriniz. ");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Girdiğiniz sayı , belirlenen sınırdan daha büyük ya da daha küçük!");
+            }
+            finally
+            {
+
+            }
+        }
+        Console.WriteLine("İşlem Tamamlandı");
+
+    }
+}
